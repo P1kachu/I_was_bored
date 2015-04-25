5b834be39eda7f901ece213ffd8cbf33af5abbfe
Stanislas Lejay <stanislas.lejay@epita.fr>
2015-04-25 01:57:28 +0100

    Added XKCD and Compressor

---
+++ Fake_Zip_Compressor/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void openButton_Click_1(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                File.Delete(openFileDialog1.FileName);
+            MessageBox.Show("Oops, compression failed... :/");
+        }
+    }
+}
