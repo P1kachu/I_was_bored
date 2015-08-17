5330ad8c1d243891b98903e24b22286bb4e98424
Stanislas Lejay <stanislas.lejay@amundi.com>
2015-08-17 10:18:49 +0200

    sometimes you just herp the derp so hard it herpderps

--- Random_Scripts/fold_ticket.sh
+++ Random_Scripts/fold_ticket.sh
@@ -4,7 +4,13 @@ RED="\33[31;1m"
 CYAN="\33[36;1m"
 NONE="\33[0m"
 
+SIGNATURE="\nStanislas 'P1kachu' Lejay
+\nEPITA 2018
+\nIf you're sleeping, you're doing it wrong\n"
+
+
 echo -e $RED"Input text: "$NONE
 read TEXT
 echo -e $CYAN"Output ticket: "$NONE
-echo TEXT | fold -ws 80
+echo TEXT | fold -ws 70
+echo -e $SIGNATURE
