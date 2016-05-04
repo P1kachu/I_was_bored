d0bf7949014dc519512261fa06adc4cc6662c66b
Stanislas Lejay <stanislas.lejay@epita.fr>
2016-05-04 16:49:30 +0200

    PEBKAC

---
+++ Random_Scripts/qee.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python3
+
+################################################################################
+#
+# QEE - Quickly Extract Everything
+# P1kachu 2016
+# Small utility to extract every archives file at once in a folder
+#
+################################################################################
+
+import os
+import argparse
+import tarfile
+import subprocess as sub
+
+
+def red(string):
+    return "\033[31;1m{0}\033[0m".format(string)
+
+def green(string):
+    return "\033[32;1m{0}\033[0m".format(string)
+
+
+def parse_file_output(output):
+    output = output.decode("utf-8").rstrip()
+    semicolon = output.find(':') + 2
+    output = output[semicolon:]
+    return output
+
+
+def print_verbose(string):
+    if not args.silent:
+        print(string)
+
+
+def handle_tarxz(f):
+    print_verbose("{0} Handling TAR/XZ file ({1})".format(green("[+]"), f))
+    try:
+        tar = tarfile.open(f)
+        tar.extractall()
+        tar.close()
+    except:
+        print("{0} failed to open {1}".format(red("[-]"), f))
+
+def decompress(args):
+    if args.file:
+        files = args.file
+    else:
+        files = [f for f in os.listdir('.') if os.path.isfile(f)]
+
+    for f in files:
+        p = sub.Popen(['file', f],stdout=sub.PIPE,stderr=sub.PIPE)
+        output, errors = p.communicate()
+        output = parse_file_output(output).lower()[:80]
+
+
+        # ADD EXTENSIONS HERE
+        if "tar" in  output:
+            handle_tarxz(f)
+        elif "xz" in output:
+            handle_tarxz(f)
+        else:
+            print_verbose("[ ] Unkown file type: {0}".format(output))
+
+
+
+if __name__ in "__main__":
+    parser = argparse.ArgumentParser(description="")
+    parser.add_argument('-s', '--silent', help='Sets verbose mode off', action='store_true')
+    parser.add_argument('-f', '--file', help='file to extract')
+    args = parser.parse_args()
+
+    decompress(args)
+
+
