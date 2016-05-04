b095b366fd06d8bdbb9d4d3973208fe811614649
Stanislas Lejay <stanislas.lejay@epita.fr>
2016-05-04 18:14:17 +0200

    various changes

--- Random_Scripts/qee.py
+++ Random_Scripts/qee.py
@@ -12,7 +12,9 @@ import os
 import argparse
 import tarfile
 import subprocess as sub
+import contextlib
 import shutil
+import lzma
 
 OUTPUT_DIR = "qee_output"
 
@@ -42,43 +44,81 @@ def handle_tarxzgzip(f):
         tar = tarfile.open(f)
         tar.extractall(OUTPUT_DIR)
         tar.close()
-    except:
-        print("{0} failed to open {1}".format(red("[-]"), f))
+        print("{0} Extraction done: {1}".format(green('[+]'), f))
+    except Exception as e:
+        # Some files are tar.xz, others are only .xz
+        # Handling of .xz here
+        try:
+            with lzma.open(f) as f2:
+                out = open('{0}/{1}_extracted'.format(OUTPUT_DIR, f), 'wb')
+                out.write(f2.read())
+        except Exception as e2:
+            print("{0} Extraction fail: {1}".format(red("[-]"), f))
+            print_verbose("{0} Error: {1}".format(red("[-]"), e2))
+            return 1
+    return 0
+
+
+def stats(nb_of_files, nb_of_fails, nb_of_unknowns):
+    print("")
+    success = nb_of_files - nb_of_fails - nb_of_unknowns
+    p = success / nb_of_files * 100
+    print("[ ] Archives opened: {0}/{1} ({2}%)".format(success, nb_of_files, p))
+    print("[ ] Files with unkown extensions: {0}".format(nb_of_unknowns))
+
+
+def cut_output(output):
+    if len(output) > 80:
+        output = output[:80] + "..."
+
+    return output
 
 def decompress(args):
+
+    # Every file or only one
     if args.file:
         files = [args.file]
     else:
         files = [f for f in os.listdir('.') if os.path.isfile(f)]
 
+    # Output directory
     if args.override:
         shutil.rmtree(OUTPUT_DIR)
-
-
     if not os.path.exists(OUTPUT_DIR):
         os.makedirs(OUTPUT_DIR)
     else:
-        print("{0} Directory '{1}' already exists. Rename it or delete it".format(
-            red('[-]'), OUTPUT_DIR))
+        print("{0} Directory '{1}' already exists.".format(red('[-]'), OUTPUT_DIR))
+        print("{0} Relaunch with '-o' to override".format(red('[-]')))
         exit(-1)
 
+    print_verbose("[ ] Extracting {0} files to {1}".format(len(files), OUTPUT_DIR))
+    print_verbose("")
 
+    nb_of_fails    = 0
+    nb_of_unknowns = 0
     for f in files:
         p = sub.Popen(['file', f],stdout=sub.PIPE,stderr=sub.PIPE)
         output, errors = p.communicate()
-        output = parse_file_output(output).lower()[:80]
+        output = parse_file_output(output).lower()
+        output = cut_output(output)
 
-
-               # ADD EXTENSIONS HERE
+        # ADD EXTENSIONS HERE
+        # Could factorise this,
+        # but easier to read like
+        # that
         if "tar" in  output:
-            handle_tarxzgzip(f)
+            nb_of_fails += handle_tarxzgzip(f)
         elif "xz" in output:
-            handle_tarxzgzip(f)
+            nb_of_fails += handle_tarxzgzip(f)
         elif "gzip" in output:
-            handle_tarxzgzip(f)
+            nb_of_fails += handle_tarxzgzip(f)
         else:
-            print_verbose("[ ] Unkown file type: {0}".format(output))
+            nb_of_unknowns += 1
+            print_verbose("[ ] {0}: Unkown file type ({1})".format(f, output))
 
+        print_verbose("-" * 82)
+
+    stats(len(files), nb_of_fails, nb_of_unknowns)
 
 
 if __name__ in "__main__":
